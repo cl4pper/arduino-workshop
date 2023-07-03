4c87fe8eb5d0eda2c19c7b3e55624018db41a1af
Souza, Matheus <cl4pper@gmail.com>
2023-07-03 11:28:58 +0100

    feat: function to switch values between pins

--- alternate_duo/alternate_duo.ino
+++ alternate_duo/alternate_duo.ino
@@ -1,27 +1,30 @@
 int PIN_A = 9;
 int PIN_B = 10;
-int TIMER = 1000;
+int TIMER = 500;
 
-void switchA() {
-  digitalWrite(PIN_A, HIGH);
-  digitalWrite(PIN_B, LOW);
-  delay(TIMER);
-}
+//switches values between pins - A, B, etc
+void switcher(char pin) {
+  if (pin == 'A') {
+    digitalWrite(PIN_A, HIGH);
+    digitalWrite(PIN_B, LOW);
+    delay(TIMER);
+  }
 
-void switchB() {
-  digitalWrite(PIN_A, LOW);
-  digitalWrite(PIN_B, HIGH);
-  delay(TIMER);
+  else if (pin == 'B') {
+    digitalWrite(PIN_A, LOW);
+    digitalWrite(PIN_B, HIGH);
+    delay(TIMER);
+  }
+
+  // else...
 }
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(PIN_A, OUTPUT);
   pinMode(PIN_B, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  switchA();
-  switchB();
+  switcher('A');
+  switcher('B');
 }
